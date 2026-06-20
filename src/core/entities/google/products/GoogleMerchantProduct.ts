2fc32f536318d4547c18ade0a65ca7a9227285a4
Arturo Gutierrez <agutierrez.gyo@gmail.com>
2026-06-20 16:08:06 -0300

    Feature new process

--- src/core/entities/google/products/GoogleMerchantProduct.ts
+++ src/core/entities/google/products/GoogleMerchantProduct.ts
@@ -1,3 +1,13 @@
+export interface GoogleMerchantShipping {
+  country: string;
+  service?: string;
+  price?: number;
+  minHandlingTime?: number;
+  maxHandlingTime?: number;
+  minTransitTime?: number;
+  maxTransitTime?: number;
+}
+
 export interface GoogleMerchantProduct {
   sku: string;
   title: string;
@@ -7,4 +17,11 @@ export interface GoogleMerchantProduct {
   brand?: string;
   imageUrl: string;
   productUrl: string;
+  additionalImageUrls?: string[];
+  condition?: string;
+  googleProductCategory?: string;
+  gtins?: string[];
+  mpn?: string;
+  identifierExists?: boolean;
+  shipping?: GoogleMerchantShipping[];
 }
